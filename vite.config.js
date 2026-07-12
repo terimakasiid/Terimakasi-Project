cf5eb603e24d4c59457584df24dc859184c038ca
terimakasiid <terimakasi.id@gmail.com>
2026-07-12 18:26:29 +0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -27,12 +27,6 @@ export default defineConfig({
             sizes: "512x512",
             type: "image/png",
           },
-          {
-            src: "/icon-512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
         ],
       },
     }),
